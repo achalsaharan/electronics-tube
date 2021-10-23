aac0db211bad4cbc2901ec41685120307c4f861b
achal saharan <asaharan_be17@thapar.edu>
2021-10-23 12:37:46 +0530

    update backend api url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-//export const API = 'https://video-library-backend.herokuapp.com';
-export const API = 'http://localhost:3998';
+export const API = 'https://video-library-backend.herokuapp.com';
+// export const API = 'http://localhost:3998';
